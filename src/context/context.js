69cd0ec853b1d005b2834735bfc7347110c48d84
Timilehin Arigbede <harriemannie@gmail.com>
2022-10-10 21:41:44 +0100

    Toggle Sidebar

--- src/context/context.js
+++ src/context/context.js
@@ -4,10 +4,20 @@ import sublinks from '../constants/links'
 const GatsbyContext = React.createContext()
 
 const GatsbyProvider = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
   const [links, setLinks] = useState(sublinks)
+
+  const showSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+
+  const hideSidebar = () => {
+    setIsSidebarOpen(false)
+  }
   return (
-    <GatsbyContext.Provider value={{ isSidebarOpen, links }}>
+    <GatsbyContext.Provider
+      value={{ isSidebarOpen, links, showSidebar, hideSidebar }}
+    >
       {children}
     </GatsbyContext.Provider>
   )
