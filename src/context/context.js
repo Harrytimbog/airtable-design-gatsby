5dc607c0e6aab5a35d004f32a624d6550f0c0495
Timilehin Arigbede <harriemannie@gmail.com>
2022-10-10 17:54:56 +0100

    Context API

--- src/context/context.js
+++ src/context/context.js
@@ -1,3 +1,14 @@
 import React, { useState } from 'react'
 import sublinks from '../constants/links'
 
+const GatsbyContext = React.createContext()
+
+const GatsbyProvider = ({ children }) => {
+  return (
+    <GatsbyContext.Provider value="Hello world">
+      {children}
+    </GatsbyContext.Provider>
+  )
+}
+
+export { GatsbyContext, GatsbyProvider }
